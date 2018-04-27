89a448201e7991fac032bd07718747ebf2c1b785
ISHVEER <ishveersingh93@gmail.com>
2018-04-27 13:30:56 +0530

    initialized repo

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+
+import Login from './src/screens/Login';
+import Secured from './src/screens/Secured';
+
+class App extends Component {
+
+  state = {
+    isLoggedIn: false
+  }
+
+  render() {
+
+    if (this.state.isLoggedIn) 
+      return <Secured 
+          onLogoutPress={() => this.setState({isLoggedIn: false})}
+        />;
+    else 
+      return <Login 
+          onLoginPress={() => this.setState({isLoggedIn: true})}
+        />;
+  }
+
+}
+
+export default App;
